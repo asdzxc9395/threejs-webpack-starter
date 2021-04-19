6eafba1347c42bbd33aa6ebff9b1bf7bb5d9e2ec
chaejinho <asdzxc9395@naver.com>
2021-04-19 16:58:38 +0900

    2021-04-09 triangle and square
    .
    k;
    
    dld;asfk

--- webgl/readme.md
+++ webgl/readme.md
@@ -1,6 +1,6 @@
 # Three.js Starter
 
-https://webglfundamentals.org/webgl/lessons/ko/webgl-fundamentals.html
+https://webgl2fundamentals.org/webgl/lessons/ko/webgl-fundamentals.html
 
 WEBGL은 종종 3D API로 치부된다. 하지만 WEBGL은 rasterization 엔진에 불과할 뿐이다. 점 선 면등을 webgl을
 통해서 그리는데 이 세가지를 통해 3d를 만드는 것이다
@@ -8,8 +8,11 @@ WEBGL은 종종 3D API로 치부된다. 하지만 WEBGL은 rasterization 엔진�
 webgl은 컴퓨터에 있는 gpu에서 실행됩니다. 따라서 gpu에서 실행되는 코드를 제공해야 한다. 해당 코드는 두 종류가
 제공되는데 (vertex shader, fragment shader) 이 두개를 합쳐서 구현을 해야 한다.
 
-Vertex shader은 정점 위치를 계산하여야 하는데 점, 선, 삼각형 등을 포함한 다양한 종류의 기본형타입을 레스터화 할수 있다.
-Fragment shader은 현재 그러지는 primitive의 각 픽셀에 대한 색상을 계산한다.
+vertex shader들의 역할은 vertex위치를 계산 하는 것입니다. 함수가 출력하는 위치를 기준으로 WebGL은 점 선 및 삼각형을 비롯한 다양한 종류의 기본요소(primitives)들을 래스터화 할수 있습니다. 
+
+이 기본 요소들을 레스터화 할 때 사용자가 두 번째로 제공한 프레그먼트 셰이더(fragment shader)를 호출합니다. 프래그먼트 셰이더(fragment shader)의 역할은 현재 그려져 있는 기본 요소(primitive)의 각 픽셀 색상을 계산하는 것입니다.
+
+거의 모든 WebGL API는 이러한 함수 쌍을 실행 하기 위한 상태를 설정 하는것과 관련이 있습니다. 그리기 원하는 것마다 여러 상태를 설정 한 다음 GPU에서 쉐이더를 실행하는gl.drawArrays 또는 gl.drawElements를 호출하여 프로그램(program) 을 실행해야 합니다.
 
 대부분의 webgl은 이러한 함수들을 실행햐기 위해 세팅을 해야 하는데, gpu에서 shader를 실행하는
 gl.drawArrays나 gl.drawElement를 호출해서 함수들을 실행해야 한다.
--- webgl/triangle/script.js
+++ webgl/triangle/script.js
@@ -1,38 +1,31 @@
 "use strict";
 
 var vertexShaderSource = `#version 300 es
- 
-// attribute는 버텍스 쉐이더에 대한 입력(in)입니다.
-// 버퍼로부터 받은 데이터입니다.
-in vec2 a_position;
- 
-uniform vec2 u_resolution;
 
+// an attribute is an input (in) to a vertex shader.
+// It will receive data from a buffer
+in vec4 a_position;
+
+// all shaders have a main function
 void main() {
-  // 픽셀 위치를 0.0에서 1.0 사이로 변환합니다.
-  vec2 zeroToOne = a_position / u_resolution;
 
-  // 0 -> 1에서 0 -> 2로 변환
-  vec2 zeroToTwo = zeroToOne * 2.0;
-
-  // 0 -> 2 에서 -1 -> +1 변환(클립 공간)
-  vec2 clipSpace = zeroToTwo - 1.0;
-
-  gl_Position = vec4(clipSpace, 0, 1);
+  // gl_Position is a special variable a vertex shader
+  // is responsible for setting
+  gl_Position = a_position;
 }
 `;
 
 var fragmentShaderSource = `#version 300 es
- 
-// 프래그먼트 쉐이더는 기본 정밀도를 가지고 있지 않으므로 선언을 해야합니다.
-// highp는 기본값으로 적당합니다. "높은 정밀도"를 의미합니다.
+
+// fragment shaders don't have a default precision so we need
+// to pick one. highp is a good default. It means "high precision"
 precision highp float;
- 
-// 프래그먼트 쉐이더(fragment shader)에서 출력을 선언 해야합니다.
+
+// we need to declare an output for the fragment shader
 out vec4 outColor;
- 
+
 void main() {
-  // 붉은-보라색으로 출력하게 설정합니다.
+  // Just set the output to a constant redish-purple
   outColor = vec4(1, 0, 0.5, 1);
 }
 `;
@@ -71,7 +64,6 @@ function main() {
   var canvas = document.querySelector("#c");
   var gl = canvas.getContext("webgl2");
   if (!gl) {
-    // webgl2를 사용할수 없습니다!
     return;
   }
 
@@ -85,9 +77,6 @@ function main() {
   // look up where the vertex data needs to go.
   var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 
-  // look up uniform locations
-  var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
-
   // Create a buffer and put three 2d clip space points in it
   var positionBuffer = gl.createBuffer();
 
@@ -95,12 +84,9 @@ function main() {
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
   var positions = [
-    10, 20,
-    80, 20,
-    10, 80,
-    10, 80,
-    80, 20,
-    80, 80,
+    0, 0,
+    0, 0.5,
+    0.7, 0,
   ];
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
@@ -114,11 +100,11 @@ function main() {
   gl.enableVertexAttribArray(positionAttributeLocation);
 
   // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-  var size = 2;          // 한번 실행할 때마다 2개 구성 요소 사용
-  var type = gl.FLOAT;   // 데이터는 32비트 소수점
-  var normalize = false; // 정규화 되지 않은 데이터
-  var stride = 0;        // 0 은 실행할 때마다 `size * sizeof(type)`만큼 다음 위치로 이동합니다.
-  var offset = 0;        // 버퍼의 첫 위치부터 시작
+  var size = 2;          // 2 components per iteration
+  var type = gl.FLOAT;   // the data is 32bit floats
+  var normalize = false; // don't normalize the data
+  var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+  var offset = 0;        // start at the beginning of the buffer
   gl.vertexAttribPointer(
       positionAttributeLocation, size, type, normalize, stride, offset);
 
@@ -134,18 +120,13 @@ function main() {
   // Tell it to use our program (pair of shaders)
   gl.useProgram(program);
 
-  // 원하는 attribute/buffer 집합을 바인드합니다.
+  // Bind the attribute/buffer set we want.
   gl.bindVertexArray(vao);
-
-  // Pass in the canvas resolution so we can convert from
-  // pixels to clipspace in the shader
-  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
-
 
   // draw
   var primitiveType = gl.TRIANGLES;
   var offset = 0;
-  var count = 6;
+  var count = 3;
   gl.drawArrays(primitiveType, offset, count);
 }
 
